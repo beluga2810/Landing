a6dd2de7ee4ece542ebd3b580cb6a77f3acf8f9d
beluga2810 <voitovitchegorushka@yandex.by>
2023-10-04 20:09:23 +0300

    feat: add all functionality

---
+++ index.js
@@ -0,0 +1,234 @@
+const i18Obj = {
+    'en' : {
+        'nav__link-company' : 'Company',
+        'nav__link-portfolio' : 'Portfolio',
+        'nav__link-catalog' : 'Catalog',
+        'nav__link-service' : 'Service',
+        'nav__link-contact' : 'Contact',
+        'intro__title-title' : 'Lighting equipment in St. Petersburg',
+        'intro__subtitle-subtitle' : 'Sale, rental of lighting equipment. Registration of officialevents on a turnkey basis.',
+        'intro__btn-link-link' : 'Go over',
+        'proects__title-title' : 'Our projects',
+        'proects__about-title-title1' : 'ssk Olympus',
+        'proects__about-subtitle-subtitle1' : 'Lighting design of the forum “Fur Coats-2019',
+        'proects__about-title-title' : 'ssk Olympus',
+        'proects__about-subtitle-subtitle' : 'Lighting design of the forum “Fur Coats-2019',
+        'catalog__title-title' : 'Product catalog',
+        'catalog__tab-link-link1' : 'Robe',
+        'catalog__tab-link-link2' : 'Compulite',
+        'catalog__item-about-1title' : 'Static LED devices',
+        'catalog__item-about-2title' : 'Controllers',
+        'catalog__item-about-3title' : 'Accessories',
+        'rent__title-title' : 'Equipment rental',
+        'rent__description1' : 'We are engaged in the rental of lighting and stage equipment from leading world manufacturers, such as Robe, SGM, StudioDue, ChainMaster,Milos, Universal Effects, ArtLighting, MobilTech, Lampo, Martin,Spotlight, Compulite, Eurolite, etc.',
+        'slider__title-title' : 'New equipment provided for rent:',
+        'slider__list-title1' : 'Robe Robin ParFect 100',
+        'slider__list-item1' : 'Light source 12 RGBW LEDs of 15 watts each',
+        'slider__list-item2' : 'Opening angle 7°',
+        'slider__list-item3' : 'A 40° diffusing lens is included',
+        'slider__list-item4' : 'Power consumption 200 W',
+        'news__title-title' : 'News',
+        'news__btn-link-link' : 'all news',
+        'news__item-title-title' : 'Milos Workshop',
+        'news__item-title-title1' : 'Our employees have passed the last seminar from Milos. They received a new amount of knowledge that will allow our company to equip not only events, but also production facilities.',
+        'news__link-link' : 'Read completely...',
+        'news__item-title-1title' : 'Prolight+Sound NAMM Russia 2016 Exhibition',
+        'p-text' : '30 contracts, thousands of enthusiastic visitors...',
+        'news__item-title-2title' : 'SEMINAR ROBE',
+        'P-text2' : 'On January 30, 2017, our employees visited Berlin..',
+        'footer_contact' : '191180, St. Petersburg, Fontanka River Embankment, 90, building 1',
+        'footer__link-company' : 'Company',
+        'footer__link-catalog' : 'Catalog',
+        'footer__link-proect' : 'Project',
+        'footer__link-news' : 'News',
+        'footer__link-rent' : 'Rent',
+        'footer__link-service' : 'Service',
+        'footer__link-translite' : '© 2019 CJSC "Translight". All rights reserved.',
+        'footer__link-den' : 'Development and design - D-e-n.ru',
+    }, 
+    'ru':{
+        'nav__link-company' : 'О компании',
+        'nav__link-portfolio' : 'Портфолио',
+        'nav__link-catalog' : 'Каталог',
+        'nav__link-service' : 'Сервис',
+        'nav__link-contact' : 'Контакты',
+        'intro__title-title' : 'СВЕТОТЕХНИЧЕСКОЕ ОБОРУДОВАНИЕ В ПЕТЕРБУРГЕ',
+        'intro__subtitle-subtitle' : 'Продажа, аренда светотехнического оборудования. Оформление официальных мероприятий под ключ.',
+        'intro__btn-link-link' : 'ПЕРЕЙТИ',
+        'proects__title-title' : 'НАШИ ПРОЕКТЫ',
+        'proects__about-title-title1' : 'ССК “ОЛИМП”',
+        'proects__about-subtitle-subtitle1' : 'Световое оформление форума “Шубы-2019”',
+        'proects__about-title-title' : 'ССК “ОЛИМП”',
+        'proects__about-subtitle-subtitle' : 'Световое оформление форума “Шубы-2019”',
+        'catalog__title-title' : 'КАТАЛОГ ПРОДУКЦИИ',
+        'catalog__tab-link-link1' : 'Robe',
+        'catalog__tab-link-link2' : 'Compulite',
+        'catalog__item-about-1title' : 'СТАТИЧНЫЕ СВЕТОДИОДНЫЕ ПРИБОРЫ',
+        'catalog__item-about-2title' : 'КОНТРОЛЛЕРЫ',
+        'catalog__item-about-3title' : 'АКСЕССУАРЫ',
+        'rent__title-title' : 'АРЕНДА ОБОРУДОВАНИЯ',
+        'rent__description1' : 'Мы занимаемся прокатом светового и сценического оборудования ведущих мировых производителей, таких как Robe, SGM, StudioDue, ChainMaster, Milos, Universal Effects, ArtLighting, MobilTech, Lampo, Martin, Spotlight, Compulite, Eurolite и др.',
+        'slider__title-title' : 'НОВОЕ ОБОРУДОВАНИЕ, ПРЕДОСТАВЛЯЕМОЕ В АРЕНДУ:',
+        'slider__list-title1' : 'Robe Robin ParFect 100',
+        'slider__list-item1' : 'Источник света 12 светодиодов RGBW по 15 Вт каждый',
+        'slider__list-item2' : 'Угол раскрытия 7°',
+        'slider__list-item3' : 'В комплекте рассеивающая линза 40°',
+        'slider__list-item4' : 'Потребляемая мощность 200 Вт',
+        'news__title-title' : 'НОВОСТИ',
+        'news__btn-link-link' : 'ВСЕ НОВОСТИ',
+        'news__item-title-title' : 'СЕМИНАР Milos',
+        'news__item-title-title1' : 'Наши сотрудники прошли последний семинар от компании Milos. Они получили новый объем знаний, который позволит нашей компании заниматься оборудованием не только мероприятий, но производственных помещений.',
+        'news__link-link' : 'Читать полностью...',
+        'news__item-title-1title' : 'ВЫСТАВКА Prolight+Sound NAMM Russia 2016',
+        'p-text' : '30 контрактов, тысячи восторженных посетителей...',
+        'news__item-title-2title' : 'СЕМИНАР ROBE',
+        'P-text2' : '30 января 2017 года наши сотрудники побывали в Берлине..',
+        'footer_contact' : '191180, Санкт-Петербург, Набережная реки Фонтанки, 90, корпус 1',
+        'footer__link-company' : 'О компании',
+        'footer__link-catalog' : 'Каталог',
+        'footer__link-proect' : 'Наши проекты',
+        'footer__link-news' : 'Новости',
+        'footer__link-rent' : 'Аренда',
+        'footer__link-service' : 'Сервисное обслуживание',
+        'footer__link-translite' : '© 2019 ЗАО «Транслайт». Все права защищены.',
+        'footer__link-den' : 'Разработка и дизайн - D-e-n.ru',
+    }
+}
+function getTranslate(language) {
+    const elements = document.querySelectorAll('[data-i18]');
+  
+    elements.forEach(element => {
+      const translationKey = element.dataset.i18;
+      const translation = i18Obj && i18Obj[language] && i18Obj[language][translationKey];
+      if (translation) {
+        element.textContent = translation;
+      }
+    });
+  }
+  
+  let currentLang = 'en';
+  
+  function toggleLanguage() {
+    if (currentLang === 'en') {
+      getTranslate('ru');
+      currentLang = 'ru';
+      localStorage.setItem('language', 'ru');
+    } else {
+      getTranslate('en');
+      currentLang = 'en';
+      localStorage.setItem('language', 'en');
+    }
+  }
+  
+  const lang_btn = document.querySelector('.lang-btn');
+  lang_btn.addEventListener('click', toggleLanguage);
+
+  const root = document.documentElement;
+  const button = document.querySelector('.mode-btn');
+  
+  button.addEventListener('click', function () {
+    const root = document.documentElement;
+    if (button.textContent === 'light') {
+      setDarkTheme(root);
+      button.textContent = 'dark';
+      localStorage.setItem('theme', 'dark');
+    } else {
+      setLightTheme(root);
+      button.textContent = 'light';
+      localStorage.setItem('theme', 'light');
+    }
+  });
+
+  function setDarkTheme(root) {
+    root.style.setProperty('--text-color', 'white');
+    root.style.setProperty('--bg-color', '#08101d');
+  }
+  
+  function setLightTheme(root) {
+      root.style.setProperty('--text-color', 'black');
+      root.style.setProperty('--bg-color', '#e1e3f5');
+  }
+  
+  function getLocalStorage(){
+    if (localStorage.getItem('theme') === 'dark') {
+      const root = document.documentElement;
+      setDarkTheme(root);
+      button.textContent = 'dark';
+    } else {
+      const root = document.documentElement;
+      setLightTheme(root);
+      button.textContent = 'light';
+    }
+    if (localStorage.getItem('language') === 'ru') {
+      getTranslate('ru');
+      currentLang = 'ru';
+    } else {
+      getTranslate('en');
+      currentLang = 'en';
+    }
+  }
+  window.addEventListener('load', getLocalStorage);
+
+
+
+
+// const elem = document.querySelectorAll('.intro')
+// for(i=0;i<elem.length;i++){
+//   elem[i].onclick = fincShow;
+// }
+// function fincShow(){
+//   this.classList.toggle('intro-show');
+// }
+  
+
+
+const elem = document.querySelectorAll('.intro');
+for (let i = 0; i < elem.length; i++) {
+  elem[i].onclick = fincShow;
+}
+function fincShow() {
+  this.classList.toggle('intro-show');
+  saveState();
+}
+
+function saveState() {
+  const states = [];
+  for (let i = 0; i < elem.length; i++) {
+    states.push({
+      id: i,
+      isIntroShow: elem[i].classList.contains('intro-show'),
+    });
+  }
+  localStorage.setItem('introStates', JSON.stringify(states));
+}
+function restoreState() {
+  const savedStates = localStorage.getItem('introStates');
+  if (savedStates) {
+    const states = JSON.parse(savedStates);
+    for (let i = 0; i < states.length; i++) {
+      const state = states[i];
+      const element = elem[state.id];
+
+      if (state.isIntroShow) {
+        element.classList.add('intro-show');
+      } else {
+        element.classList.remove('intro-show');
+      }
+    }
+  }
+}
+
+restoreState();
+
+
+
+const audioPlayer = document.querySelector('.audioPlayer');
+const playButton = document.querySelector('.audio-btn');
+
+playButton.addEventListener('click', function() {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+  } else {
+    audioPlayer.pause();
+  }
+});
